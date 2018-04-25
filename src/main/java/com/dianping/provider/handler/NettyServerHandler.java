5443cfd6dacc967c940d7b56c919300c80fcde56
ting.fang <ting.fang@dianping.com>
2018-04-25 21:31:32 +0800

    all

--- src/main/java/com/dianping/provider/handler/NettyServerHandler.java
+++ src/main/java/com/dianping/provider/handler/NettyServerHandler.java
@@ -1,10 +1,17 @@
 package com.dianping.provider.handler;
 
-import io.netty.channel.ChannelHandlerAdapter;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 
 /**
  * Created by fang on 2018/4/25.
  */
-public class NettyServerHandler extends ChannelHandlerAdapter {
+public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("服务端接收消息" + msg);
+    }
 
 }
