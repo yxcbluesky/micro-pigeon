76f5015723ebb0033c42927ee5d7d2a7f0722f9c
ting.fang <ting.fang@dianping.com>
2018-04-26 09:43:53 +0800

    all

--- src/main/java/com/dianping/ServiceFactory.java
+++ src/main/java/com/dianping/ServiceFactory.java
@@ -1,11 +1,16 @@
 package com.dianping;
 
+import com.dianping.remoting.invoker.config.InvokerConfig;
+import com.dianping.remoting.invoker.proxy.ServiceProxy;
+
 /**
  * Created by fang on 2018/4/25.
  */
 public class ServiceFactory {
 
+    private static final ServiceProxy proxy = new ServiceProxy();
+
     public static <T> T getService(Class<T> serviceInterface) {
-        return null;
+        return proxy.getProxy(new InvokerConfig<T>(serviceInterface));
     }
 }
--- src/main/java/com/dianping/remoting/invoker/proxy/ServiceProxy.java
+++ src/main/java/com/dianping/remoting/invoker/proxy/ServiceProxy.java
@@ -1,7 +1,27 @@
 package com.dianping.remoting.invoker.proxy;
 
+import com.dianping.remoting.invoker.config.InvokerConfig;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * Created by fang on 2018/4/26.
  */
 public class ServiceProxy {
+
+    /**
+     * 保存已创建的代理类对象
+     */
+    private static final Map<InvokerConfig<?>, Object> services = new ConcurrentHashMap<>();
+
+    public <T> T getProxy(InvokerConfig<T> invokerConfig) {
+
+        Object service = services.get(invokerConfig);
+
+        if (service == null) {
+
+        }
+        return null;
+    }
 }
