d600505522108f1c2c824a0e81daa89d7f975a63
ting.fang <ting.fang@dianping.com>
2018-04-26 21:06:32 +0800

    all

--- src/main/java/com/dianping/remoting/common/process/ServiceInvocationHandler.java
+++ src/main/java/com/dianping/remoting/common/process/ServiceInvocationHandler.java
@@ -1,10 +1,11 @@
 package com.dianping.remoting.common.process;
 
 import com.dianping.remoting.common.domain.InvocationContext;
+import com.dianping.remoting.common.domain.InvocationResponse;
 
 /**
  * Created by fang on 2018/4/26.
  */
 public interface ServiceInvocationHandler {
-    void handle(InvocationContext ctx);
+    InvocationResponse handle(InvocationContext ctx);
 }
---
+++ src/main/java/com/dianping/remoting/invoker/process/InvokerProcessHandlerFactory.java
@@ -0,0 +1,8 @@
+package com.dianping.remoting.invoker.process;
+
+/**
+ * Created by fang on 2018/4/26.
+ */
+public class InvokerProcessHandlerFactory {
+    public
+}
