633d92e7a0fa3ca0909bd7ca5db03280fd6d62d5
ting.fang <ting.fang@dianping.com>
2018-04-27 20:30:35 +0800

    all

--- src/main/java/com/dianping/remoting/common/domain/InvocationResponse.java
+++ src/main/java/com/dianping/remoting/common/domain/InvocationResponse.java
@@ -4,4 +4,5 @@ package com.dianping.remoting.common.domain;
  * Created by fang on 2018/4/26.
  */
 public interface InvocationResponse {
+    InvocationResponse getResponse();
 }
---
+++ src/main/java/com/dianping/remoting/invoker/config/InvokerContext.java
@@ -0,0 +1,9 @@
+package com.dianping.remoting.invoker.config;
+
+import com.dianping.remoting.common.domain.InvocationContext;
+
+/**
+ * Created by fang on 2018/4/27.
+ */
+public class InvokerContext implements InvocationContext {
+}
