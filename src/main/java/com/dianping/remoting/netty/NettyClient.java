892b17d21120e3b13721eac9f5a144b1be7cf70f
ting.fang <ting.fang@dianping.com>
2018-04-27 20:44:15 +0800

    all

---
+++ src/main/java/com/dianping/remoting/netty/NettyClient.java
@@ -0,0 +1,29 @@
+package com.dianping.remoting.netty;
+
+import com.dianping.remoting.invoker.Client;
+
+/**
+ * Created by fang on 2018/4/27.
+ */
+public class NettyClient implements Client {
+
+    @Override
+    public void open() {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public String getHost() {
+        return null;
+    }
+
+    @Override
+    public int getPort() {
+        return 0;
+    }
+}
