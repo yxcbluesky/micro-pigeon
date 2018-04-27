e84e3be4900eda6bf7abc24f81d655a0d7c5021b
ting.fang <ting.fang@dianping.com>
2018-04-27 20:45:11 +0800

    all

--- src/main/java/com/dianping/remoting/invoker/config/InvokerContext.java
+++ src/main/java/com/dianping/remoting/invoker/config/InvokerContext.java
@@ -1,10 +1,15 @@
 package com.dianping.remoting.invoker.config;
 
 import com.dianping.remoting.common.domain.InvocationContext;
+import com.dianping.remoting.common.domain.InvocationRequest;
+import com.dianping.remoting.common.domain.InvocationResponse;
+import com.dianping.remoting.invoker.Client;
 
 /**
  * Created by fang on 2018/4/27.
  */
 public class InvokerContext implements InvocationContext {
-
+    private Client client;
+    private InvocationRequest request;
+    private InvocationResponse response;
 }
