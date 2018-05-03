f73431654d85b48d4f1f39ed512633fea6034be3
ting.fang <ting.fang@dianping.com>
2018-05-03 22:29:01 +0800

    all

--- src/main/java/com/dianping/remoting/invoker/process/filter/InvokerInvocationFilter.java
+++ src/main/java/com/dianping/remoting/invoker/process/filter/InvokerInvocationFilter.java
@@ -1,12 +1,12 @@
 package com.dianping.remoting.invoker.process.filter;
 
-import com.dianping.remoting.common.domain.InvocationContext;
 import com.dianping.remoting.common.domain.InvocationResponse;
 import com.dianping.remoting.common.process.ServiceInvocationHandler;
+import com.dianping.remoting.invoker.config.InvokerContext;
 
 /**
  * Created by fang on 2018/4/27.
  */
 public interface InvokerInvocationFilter {
-    InvocationResponse invoke(ServiceInvocationHandler handler, InvocationContext ctx);
+    InvocationResponse invoke(ServiceInvocationHandler handler, InvokerContext ctx);
 }
--- src/main/java/com/dianping/remoting/invoker/process/filter/RemoteCallInvocationFilter.java
+++ src/main/java/com/dianping/remoting/invoker/process/filter/RemoteCallInvocationFilter.java
@@ -1,8 +1,8 @@
 package com.dianping.remoting.invoker.process.filter;
 
-import com.dianping.remoting.common.domain.InvocationContext;
 import com.dianping.remoting.common.domain.InvocationResponse;
 import com.dianping.remoting.common.process.ServiceInvocationHandler;
+import com.dianping.remoting.invoker.config.InvokerContext;
 
 /**
  * Created by fang on 2018/4/27.
@@ -10,7 +10,7 @@ import com.dianping.remoting.common.process.ServiceInvocationHandler;
 public class RemoteCallInvocationFilter implements InvokerInvocationFilter {
 
     @Override
-    public InvocationResponse invoke(ServiceInvocationHandler handler, InvocationContext ctx) {
+    public InvocationResponse invoke(ServiceInvocationHandler handler, InvokerContext ctx) {
         return null;
     }
 }
