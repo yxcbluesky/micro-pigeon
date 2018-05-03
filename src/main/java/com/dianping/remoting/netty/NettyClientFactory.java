a68b6147deb33bad8276b69eabee3542d4b03f50
ting.fang <ting.fang@dianping.com>
2018-05-03 22:18:30 +0800

    all

--- src/main/java/com/dianping/remoting/netty/NettyClientFactory.java
+++ src/main/java/com/dianping/remoting/netty/NettyClientFactory.java
@@ -2,7 +2,12 @@ package com.dianping.remoting.netty;
 
 import com.dianping.remoting.invoker.Client;
 import com.dianping.remoting.invoker.ClientConfig;
+import com.dianping.threadpool.DefaultThreadFactory;
 import org.jboss.netty.channel.ChannelFactory;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+import org.jboss.netty.channel.socket.nio.NioWorkerPool;
+
+import java.util.concurrent.Executors;
 
 /**
  * Created by fang on 2018/4/27.
@@ -11,7 +16,9 @@ public class NettyClientFactory {
 
     private volatile static ChannelFactory channelFactory = null;
 
-    public static Client create(ClientConfig clientConfig) {
+    private static ClientConfig clientConfig = new ClientConfig();
+
+    public static Client create() {
         return null;
     }
 
@@ -19,10 +26,17 @@ public class NettyClientFactory {
         if (channelFactory == null) {
             synchronized (NettyClientFactory.class) {
                 if (channelFactory == null) {
-                    channelFactory =
+                    channelFactory = new NioServerSocketChannelFactory(
+                            Executors.newCachedThreadPool(new DefaultThreadFactory("Pigeon-Netty-Client-Boss")),
+                            clientConfig.getBossThreadPoolCount(),
+                            new NioWorkerPool(
+                                    Executors.newCachedThreadPool(new DefaultThreadFactory("Pigeon-Netty-Client-Worker")),
+                                    clientConfig.getWorkerThreadPoolCount()
+                            ));
                 }
             }
         }
+        return channelFactory;
     }
 
 }
---
+++ src/main/java/com/dianping/threadpool/DefaultThreadFactory.java
@@ -0,0 +1,27 @@
+package com.dianping.threadpool;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by fang on 2018/5/3.
+ */
+public class DefaultThreadFactory implements ThreadFactory {
+
+    static final AtomicInteger poolNumber = new AtomicInteger(1);
+    final AtomicInteger threadNumber;
+    final ThreadGroup group;
+    final String namePrefix;
+
+    public DefaultThreadFactory(String preffix) {
+        this.threadNumber = new AtomicInteger(1);
+        this.group = new ThreadGroup(preffix + "-" + poolNumber.getAndIncrement() + "-threadGroup");
+        this.namePrefix = preffix + "-" + poolNumber.getAndIncrement() + "-thread-";
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+        return new Thread(this.group, r, this.namePrefix + this.threadNumber.getAndIncrement(),
+                -3715992351445876736L);
+    }
+}
