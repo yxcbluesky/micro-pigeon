ca3d4a5efe74a8efc55a98fac57175f4e2a5d0ee
ting.fang <ting.fang@dianping.com>
2018-05-14 13:12:27 +0800

    all

--- src/main/java/com/dianping/serializer/JavaSerializer.java
+++ src/main/java/com/dianping/serializer/JavaSerializer.java
@@ -1,16 +1,29 @@
 package com.dianping.serializer;
 
 import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 
 /**
  * Created by fang on 2018/5/14.
+ * jdk序列化
  */
 public class JavaSerializer implements Serializer {
 
     @Override
     public Object deserializeRequest(InputStream is) {
+        ObjectInputStream ois;
+        try {
+            ois = new ObjectInputStream(is);
+            try {
+                return ois.readObject();
+            } catch (Exception ex) {
+                ois.close();
+            }
+        } catch (Exception ex) {
+
+        }
         return null;
     }
 
